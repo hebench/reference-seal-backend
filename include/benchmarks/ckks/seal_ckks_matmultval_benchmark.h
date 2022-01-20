88c2989b7fad01fdc1251380e9b441aa5a15eb2c
Zamora Ramos, Ernesto <ernesto.zamora.ramos@intel.com>
2022-01-19 16:02:26 -0800

    Added optional number of threads for some benchmarks.
    - Added on MatMultVal and dot product CKKS.

--- include/benchmarks/ckks/seal_ckks_matmultval_benchmark.h
+++ include/benchmarks/ckks/seal_ckks_matmultval_benchmark.h
@@ -28,6 +28,9 @@ public:
     static constexpr int DefaultCoeffMudulusBits            = 45;
     static constexpr int DefaultScaleBits                   = DefaultCoeffMudulusBits;
 
+    // other workload parameters
+    static constexpr std::size_t DefaultNumThreads = 0; // 0 - use all available threads
+
     enum : std::uint64_t
     {
         Index_WParamsStart = 0,
@@ -39,7 +42,8 @@ public:
         Index_NumCoefficientModuli,
         Index_CoefficientModulusBits,
         Index_ScaleExponentBits,
-        NumWorkloadParams // This workload requires 3 parameters, and we add 4 encryption params
+        Index_NumThreads,
+        NumWorkloadParams // This workload requires 3 parameters, and we add 5 encryption params
     };
 
     MatMultValBenchmarkDescription();
@@ -119,6 +123,7 @@ private:
 
     SEALContextWrapper::Ptr m_p_ctx_wrapper;
     hebench::cpp::WorkloadParams::MatrixMultiply m_w_params;
+    int m_num_threads;
 };
 
 } // namespace ckks
